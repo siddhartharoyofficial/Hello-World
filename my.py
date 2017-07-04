2ea8642975d61606dc97a6addb8f3401ee94dd80
siddhartharoyofficial <siddhartharoyofficial@gmail.com>
2017-07-04 20:45:51 +0530

    added my.py

---
+++ my.py
@@ -0,0 +1,7 @@
+Name ="Siddhartha"
+age =20
+if age ==20 and name =="Siddhartha":
+    print "Hello this is your first github File"
+else:
+    print "No problem you on the way of creating a history"
+
